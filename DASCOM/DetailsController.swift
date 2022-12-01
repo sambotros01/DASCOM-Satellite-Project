1d377de04f6b3c62e4668b2a301d9829f826c816
albertgo98 <albertgo98@gmail.com>
2022-12-01 15:53:19 -0500

    Adding DASCOM project

---
+++ DASCOM/DetailsController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsController.swift
+//  DASCOM
+//
+//  Created by Albert Go on 11/11/22.
+//
+
+import UIKit
+
+class DetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
